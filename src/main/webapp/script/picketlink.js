fab5804ee06a195a402bc21272151e5a5eb68685
sbryzak <sbryzak@gmail.com>
2014-04-30 14:02:00 +1000

    added picketlink REST module for security services

---
+++ src/main/webapp/script/picketlink.js
@@ -0,0 +1,42 @@
+var pl = {
+  loggedIn: false,
+  account: null,
+  basePath: "",
+  createRequestObject: function(callback) {
+    var r;
+    if (window.XMLHttpRequest) {
+      r = new XMLHttpRequest();
+    } else {
+      r = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    r.onreadystatechange = function() {
+      if (r.readyState == 4) {
+        if (r.status >= 200 && r.status <= 299) {
+          // Done to avoid a memory leak
+          window.setTimeout(function() {
+            r.onreadystatechange = function() {};
+          }, 0);
+          if (callback) {
+            callback(r.responseText);
+          }
+        }
+      }
+    }
+    return r;
+  },
+  login: function(username, password, callback) {
+    var cb = function(response) {
+      if (response != null) {
+        pl.loggedIn = true;
+        pl.account = response;
+        if (callback) {
+          callback.call();
+        }
+      }
+    }
+    var r = pl.createRequestObject(cb);
+    r.open("POST", pl.basePath + "/auth/login", true);
+    r.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    r.send({username: username, password: password});
+  }
+};
