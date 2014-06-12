05a092154574a8dd94d2c0e5a133265dd3d08652
sbryzak <sbryzak@gmail.com>
2014-06-12 15:35:00 +0800

    update to picketlink 2.6.0.CR4

--- src/main/java/org/forgeide/security/SecurityConfigurationProducer.java
+++ src/main/java/org/forgeide/security/SecurityConfigurationProducer.java
@@ -18,7 +18,9 @@ import org.forgeide.security.schema.PartitionAttribute;
 import org.forgeide.security.schema.UserTokenCredential;
 import org.forgeide.security.schema.UserPasswordCredential;
 import org.forgeide.security.schema.UserIdentity;
-import org.picketlink.PartitionManagerCreateEvent;
+import org.picketlink.config.SecurityConfiguration;
+import org.picketlink.config.SecurityConfigurationBuilder;
+import org.picketlink.event.PartitionManagerCreateEvent;
 import org.picketlink.idm.PartitionManager;
 import org.picketlink.idm.config.IdentityConfiguration;
 import org.picketlink.idm.config.IdentityConfigurationBuilder;
@@ -37,7 +39,7 @@ import org.picketlink.internal.EEJPAContextInitializer;
  * 
  */
 @ApplicationScoped
-public class SecurityConfiguration
+public class SecurityConfigurationProducer
 {
    public static final String KEYSTORE_FILE_PATH = "/keystore.jks";
 
@@ -47,46 +49,51 @@ public class SecurityConfiguration
    @Inject
    private JWSTokenProvider tokenProvider;
 
-   private IdentityConfiguration identityConfig = null;
+   private SecurityConfiguration securityConfig = null;
 
    private KeyStore keyStore;
 
    @Produces
-   IdentityConfiguration createConfig()
+   SecurityConfiguration createConfig()
    {
-      if (identityConfig == null)
+      if (securityConfig == null)
       {
          initConfig();
       }
-      return identityConfig;
+      return securityConfig;
    }
 
    @SuppressWarnings("unchecked")
    private void initConfig()
    {
-      IdentityConfigurationBuilder builder = new IdentityConfigurationBuilder();
+      SecurityConfigurationBuilder builder = new SecurityConfigurationBuilder();
+      builder.identity().stateless();
 
       builder
-         .named("default")
-            .stores()
+         .identity()
+            .idmConfig()
+               .named("default")
+               .stores()
                .jpa()
-                  .mappedEntity(
-                     Partition.class,
-                     PartitionAttribute.class,
-                     IdentityType.class,
-                     IdentityAttribute.class,
-                     UserIdentity.class,
-                     UserPasswordCredential.class,
-                     UserTokenCredential.class)
-                  .supportGlobalRelationship(Relationship.class)
-                  .addContextInitializer(this.contextInitializer)
-                  .setCredentialHandlerProperty(CredentialHandler.SUPPORTED_ACCOUNT_TYPES_PROPERTY, new Class[] {User.class})
-                  .setCredentialHandlerProperty(CredentialHandler.LOGIN_NAME_PROPERTY, "username")
-                  .setCredentialHandlerProperty(TokenCredentialHandler.TOKEN_PROVIDER, tokenProvider)
-                  .supportType(User.class)
-                  .supportAllFeatures();
+               .mappedEntity(
+                        Partition.class,
+                        PartitionAttribute.class,
+                        IdentityType.class,
+                        IdentityAttribute.class,
+                        UserIdentity.class,
+                        UserPasswordCredential.class,
+                        UserTokenCredential.class)
+               .supportGlobalRelationship(Relationship.class)
+               .addContextInitializer(this.contextInitializer)
+               .setCredentialHandlerProperty(CredentialHandler.SUPPORTED_ACCOUNT_TYPES_PROPERTY,
+                        new Class[] { User.class })
+               .setCredentialHandlerProperty(CredentialHandler.LOGIN_NAME_PROPERTY, "username")
+               .setCredentialHandlerProperty(TokenCredentialHandler.TOKEN_PROVIDER, tokenProvider)
+               .supportType(User.class)
+               .supportAllFeatures();
 
-      identityConfig = builder.build();
+
+      securityConfig = builder.build();
    }
 
    public void configureDefaultPartition(@Observes PartitionManagerCreateEvent event) {
