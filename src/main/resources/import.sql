edf46a15d13a473ee80c8ea4f43af86d3adbcecf
sbryzak <sbryzak@gmail.com>
2014-12-05 17:02:29 +1000

    added rest service for new project generation

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
 insert into ProjectTemplate (id, code, name, description, archetypeGroupId, archetypeArtifactId, archetypeVersion) values (1, 'javaee', 'Java EE App', 'Java EE application based on HTML5 and REST', 'org.jboss.tools.archetypes', 'jboss-forge-html5', '1.0.0-SNAPSHOT');
 
 insert into TemplateService (id, PROJECT_TEMPLATE_ID, code, name, description, forgeCommand, steps) values (1, 1, 'jpa', 'JPA', 'Installs JPA (Java Persistence API), allowing your project to interact with a database.', 'JPA: Setup', 1);
+
+insert into TemplateService (id, PROJECT_TEMPLATE_ID, code, name, description, forgeCommand, steps) values (2, 1, 'rest', 'REST', 'Enables REST services for your project.', 'REST: Setup', 1);
